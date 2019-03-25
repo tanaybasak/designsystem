796520f9342dbe9a7c32693df79a82330b207272
Amit Kharka <kharkaa@hcl.com>
2019-03-25 18:15:41 +0530

    Added version upgrade code

--- defaultBundleName.js
+++ defaultBundleName.js
@@ -15,3 +15,22 @@ fs.readdirSync(DIST_DIR).forEach(file => {
         });
     }
 });
+
+// Update version
+const RELEASES_TYPE = process.argv[2] || 'patch';
+fs.readFile(path.join(__dirname, './package.json'), (err, fileData) => {
+    if (err) throw err;
+    const packageJsonData = JSON.parse(fileData);
+    if (packageJsonData.hasOwnProperty('version')) {
+        const version = packageJsonData.version.split('.');
+        const index = ['major', 'minor', 'patch'].indexOf(RELEASES_TYPE);
+        version[index] = Number(version[index]);
+        version[index] += 1;
+        packageJsonData.version = version.join('.');
+
+        // writing back updated package.json data
+        fs.writeFile('package.json', JSON.stringify(packageJsonData, null, 4), error => {
+            if (error) throw error;
+        });
+    }
+});
