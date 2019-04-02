6c99067ef0d9a9f1a07c2bf6787be0ce99363178
gupta-ma <gupta-ma@hcl.com>
2019-04-03 01:03:43 +0530

    feature-UICOE-55: date input and year input is working now

--- scss/components/datePicker/datePicker.js
+++ scss/components/datePicker/datePicker.js
@@ -38,6 +38,17 @@ let datePickerController = (function () {
             let date = new Date(currDateObj.year - 1, currDateObj.month, 15);
             return createDateObject(date);
         },
+
+        setDateObject: function (date) {
+            let dateArray = date.split('/');
+            return createDateObject(new Date(dateArray[2], Number(dateArray[0]) - 1, dateArray[1]));
+        },
+
+
+        getDateObject: function () {
+            return currDateObj;
+        },
+
     };
 
 })();
@@ -50,13 +61,18 @@ let UIController = (function () {
 
     let DOMstrings = {
         weekDaysPanel: '.hcl-datePicker-container-panel-weekDays',
-        DatePanel: '.hcl-datePicker-container-panel-dates',
+        datePanel: '.hcl-datePicker-container-panel-dates',
         prevMonth: '.hcl-datePicker-container-panel-month-prev',
         nextMonth: '.hcl-datePicker-container-panel-month-next',
         yearIncrease: '.hcl-datePicker-container-panel-month-selection-inputWrapper-arrows-up',
         yearDecrease: '.hcl-datePicker-container-panel-month-selection-inputWrapper-arrows-down',
         yearInput: '.hcl-datePicker-container-panel-month-selection-inputWrapper-input',
-        monthInput: '.hcl-datePicker-container-panel-month-selection-curMonth'
+        monthInput: '.hcl-datePicker-container-panel-month-selection-curMonth',
+        inputDate: '.hcl-datePicker-input',
+        inputCalSVG: '.hcl-datePicker-container-svg',
+        dateContainer: '.hcl-datePicker-container-panel',
+        showDateContainer: 'hcl-datePicker-container-panel-show',
+        dateSelected: 'hcl-datePicker-container-panel-dates-selected'
     };
 
     let getDaysInMonth = function (month, year) {
@@ -77,8 +93,8 @@ let UIController = (function () {
     let initDatePanel = function (curMonthObj) {
 
         let numOfDaysInMonth = getDaysInMonth(curMonthObj.month + 1, curMonthObj.year);
-        let html = '<span id="hcl-datePicker-%day%">%day%</span>';
-        let element = DOMstrings.DatePanel;
+        let html = '<span id="%month%/%day%/%year%">%day%</span>';
+        let element = DOMstrings.datePanel;
 
         //days from previous month
 
@@ -87,21 +103,33 @@ let UIController = (function () {
         let numOfDaysInPrevMonth = getDaysInMonth(curMonthObj.month === 0 ? 12 : curMonthObj.month, curMonthObj.month === 0 ? curMonthObj.year - 1 : curMonthObj.year);
 
         for (let i = numOfDaysInPrevMonth - numOfDaysFromPrevMonth; i <= numOfDaysInPrevMonth && numOfDaysFromPrevMonth !== 6; i++) {
-            let dayHTML = html.replaceAll('%day%', String(i));
+            let dayHTML = html.replaceAll('%day%', ('0' + String(i)).slice(-2));
+            dayHTML = dayHTML.replaceAll('%month%', ('0' + curMonthObj.month).slice(-2));
+            dayHTML = dayHTML.replaceAll('%year%', curMonthObj.year); // need to handle Jan start edge cases
             document.querySelector(element).insertAdjacentHTML('beforeend', dayHTML);
         }
 
         // days from current month
         for (let i = 1; i <= numOfDaysInMonth; i++) {
-            let dayHTML = html.replaceAll('%day%', String(i));
+            let dayHTML = html.replaceAll('%day%', ('0' + String(i)).slice(-2));
+            dayHTML = dayHTML.replaceAll('%month%', ('0' + (Number(curMonthObj.month) + 1)).slice(-2));
+            dayHTML = dayHTML.replaceAll('%year%', curMonthObj.year);
             document.querySelector(element).insertAdjacentHTML('beforeend', dayHTML);
         }
 
         // days from next month  
         for (let i = 1; i < 42 - numOfDaysInMonth - numOfDaysFromPrevMonth; i++) {
-            let dayHTML = html.replaceAll('%day%', String(i));
+            let dayHTML = html.replaceAll('%day%', ('0' + String(i)).slice(-2));
+            dayHTML = dayHTML.replaceAll('%month%', ('0' + (Number(curMonthObj.month) + 2)).slice(-2));
+            dayHTML = dayHTML.replaceAll('%year%', curMonthObj.year); // need to handle Dec last edge cases
             document.querySelector(element).insertAdjacentHTML('beforeend', dayHTML);
         }
+
+        // hightlight today's Date
+        
+        let todayDate = new Date(); //('0' + (todayDate.getMonth()+1)).slice(-2)
+        todayDate = `${('0' + (todayDate.getMonth()+1)).slice(-2)}/${todayDate.getDate()}/${todayDate.getFullYear()}`;
+        document.getElementById(todayDate)
     };
 
     String.prototype.replaceAll = function (search, replacement) {
@@ -136,9 +164,36 @@ let UIController = (function () {
         },
 
         removeExistingDates: function () {
-            let element = DOMstrings.DatePanel;
+            let element = DOMstrings.datePanel;
             document.querySelector(element).innerHTML = "";
         },
+        showDateContainer: function () {
+            let element = DOMstrings.dateContainer;
+            document.querySelector(element).classList.add(DOMstrings.showDateContainer)
+        },
+        selectDate: function (event) {
+            console.log('selectDate!!!' + event.target.id);
+            // remove highlight class if date is selected
+            // add highlist class to newly selected date 
+            document.getElementById(event.target.id).classList.add(DOMstrings.dateSelected);
+            // set value to input date field 
+            document.querySelector(DOMstrings.inputDate).value = event.target.id;
+
+
+        },
+
+        // validateDate: function (event) {
+        //     console.log('event --->>', event);
+        //     let regex = /^[0-9]{2}[\/][0-9]{2}[\/][0-9]{4}$/g;
+        //     let validDate = regex.test(event.target.value);
+        //     if (validDate) {
+        //         console.log('Valid Date')
+        //         // call select data and bring month date year into view
+        //     } else {
+        //         console.log('Invalid Date')
+        //         // show warning label
+        //     }
+        // }
 
     };
 
@@ -154,7 +209,72 @@ let controller = (function (dateCtrl, UICtrl) {
         document.querySelector(DOM.nextMonth).addEventListener('click', nextMonth);
         document.querySelector(DOM.yearIncrease).addEventListener('click', yearIncrease);
         document.querySelector(DOM.yearDecrease).addEventListener('click', yearDecrease);
+        document.querySelector(DOM.inputDate).addEventListener('click', UICtrl.showDateContainer);
+        document.querySelector(DOM.inputDate).addEventListener('change', dateChangeHandler);
+        document.querySelector(DOM.inputCalSVG).addEventListener('click', UICtrl.showDateContainer);
+        document.querySelector(DOM.yearInput).addEventListener('change', yearChangeHandler);
+        let dateElement = document.querySelector(DOM.datePanel).children;
+
+        for (let i = 0; i < dateElement.length; i++) {
+            dateElement[i].addEventListener('click', UICtrl.selectDate);
+        }
+
+        // document.getElementsByClassName(DOM.datePanel).children.forEach((node,index) =>{
+
+        //   if(index !== 0){
+        //       console.log('index',index)
+        //     node.addEventListener('click', UICtrl.selectDate(node.id));
+        //   }   
+        // });;
+
+
     };
+
+    let yearChangeHandler = function (event) {
+        let regex = /^[0-9]{4}$/g;
+        let validYear = regex.test(event.target.value);
+        
+        if (validYear) {
+            // set current Date;
+            let currDateObj = dateCtrl.getDateObject();
+            currDateObj = dateCtrl.setDateObject(`${currDateObj.month}/15/${event.target.value}`);
+            UICtrl.removeExistingDates();
+            UICtrl.initDatePanel(currDateObj);
+            UICtrl.initMonthYearPanel(currDateObj);
+
+
+            //  UICtrl.initDatePicker(dateCtrl.getCurrentMonthDetails()); // set and get current Date Obj 
+            // call select data and bring month date year into view
+        } else {
+            console.log('Invalid Date')
+            // show warning label
+        }
+
+
+    };
+
+    let dateChangeHandler = function (event) {
+        let regex = /^[0-9]{2}[\/][0-9]{2}[\/][0-9]{4}$/g;
+        let validDate = regex.test(event.target.value);
+        if (validDate) {
+            console.log('Valid Date');
+            // set current Date;
+            let currDateObj = dateCtrl.setDateObject(event.target.value)
+            UICtrl.removeExistingDates();
+            UICtrl.initDatePanel(currDateObj);
+            UICtrl.initMonthYearPanel(currDateObj);
+
+
+            //  UICtrl.initDatePicker(dateCtrl.getCurrentMonthDetails()); // set and get current Date Obj 
+            // call select data and bring month date year into view
+        } else {
+            console.log('Invalid Date')
+            // show warning label
+        }
+
+
+    };
+
 
     let prevMonth = function () {
         console.log('prevMonth Clicked !!');
@@ -164,6 +284,7 @@ let controller = (function (dateCtrl, UICtrl) {
         UICtrl.removeExistingDates();
         UICtrl.initMonthYearPanel(prevMonthObj);
         UICtrl.initDatePanel(prevMonthObj);
+        setupEventListeners();
     };
 
     let nextMonth = function () {
@@ -174,6 +295,7 @@ let controller = (function (dateCtrl, UICtrl) {
         UICtrl.removeExistingDates();
         UICtrl.initMonthYearPanel(nextMonObj);
         UICtrl.initDatePanel(nextMonObj);
+        setupEventListeners();
     };
 
     let yearIncrease = function () {
@@ -184,16 +306,18 @@ let controller = (function (dateCtrl, UICtrl) {
         UICtrl.removeExistingDates();
         UICtrl.initMonthYearPanel(incYearMonObj);
         UICtrl.initDatePanel(incYearMonObj);
+        setupEventListeners();
     };
 
     let yearDecrease = function () {
         event.stopPropagation();
-        event.preventDefault(); 
+        event.preventDefault();
         console.log('yearDecrease Clicked !!');
         let decYearMonObj = dateCtrl.getYearDecreaseMonthDetails();
         UICtrl.removeExistingDates();
         UICtrl.initMonthYearPanel(decYearMonObj);
         UICtrl.initDatePanel(decYearMonObj);
+        setupEventListeners();
     };
 
 
