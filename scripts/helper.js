755a69c587da9db55503851c0093acfe0fea1ef4
Mayank Gupta <mayankgupta@Mayanks-MacBook-Pro.local>
2019-05-09 15:04:21 +0530

    feature-UICOE-17: hemlper.js changed: let to const

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,29 +1,29 @@
 // This javascript function is for demonstation demonstration purpose of modal in landing page
 // This function will not be part package created for design system 
 
-let helperModal = (function () {
+const helperModal = (function () {
 
-    let DOMStrings = {
+    const DOMStrings = {
         modalContainer: 'hcl-modal-type',
         modalButton: 'hcl-btn-modal-type'
 
     };
 
-    let classNames = {
+    const classNames = {
        modalDisable: 'hcl-modal-hide',
     };
 
     showModal = function (event) {
-        let type = event.target.getAttribute("data-modal-type");
-        document.getElementById(`${DOMStrings.modalContainer}${type}`).classList.remove(classNames.modalDisable);
+        const modalType = event.target.getAttribute("data-modal-type");
+        document.getElementById(`${DOMStrings.modalContainer}${modalType}`).classList.remove(classNames.modalDisable);
     };
 
     hideModal = function (event) {
-        let type = event.target.closest("div[data-modal-type]").getAttribute("data-modal-type");
-        document.getElementById(`${DOMStrings.modalContainer}${type}`).classList.add(classNames.modalDisable);
+        const modalType = event.target.closest("section[data-modal-type]").getAttribute("data-modal-type");
+        document.getElementById(`${DOMStrings.modalContainer}${modalType}`).classList.add(classNames.modalDisable);
     };
 
-    let setupEventListeners = function () {
+    const setupEventListeners = function () {
         for (let type = 1; type <= 8; type++) {
             document.getElementById(`hcl-btn-modal-type${type}`).addEventListener('click', showModal);
             document.getElementById(`hcl-modal-close-type${type}`).addEventListener('click', hideModal);
