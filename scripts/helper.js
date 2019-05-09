23ff65de42b178aed42a441ae2f94e352cfd7753
Mayank Gupta <mayankgupta@Mayanks-MacBook-Pro.local>
2019-05-09 10:25:57 +0530

    feature-UICOE-172: helper.js created

---
+++ scripts/helper.js
@@ -0,0 +1,31 @@
+// this is only for deonstation purpose of maodal landing page
+// will not go in PROD 
+
+let helperOverflowMenu = (function () {
+
+    DOMStrings = {
+       
+
+    };
+
+    classNames = {
+      
+    }
+
+    showOverflowMenu = function(){
+        document.getElementById(`hcl-overflow-menu-wrap-left`ß).addEventListener('click', showOverflowMenu);
+    };
+
+    let setupEventListeners = function () {
+        document.getElementById(`hcl-overflow-ellipsis-left`).addEventListener('click', showOverflowMenu);
+    };
+
+    return {
+        setUpHeplerEvents: function () {
+            setupEventListeners();
+        }
+    };
+
+})();
+
+helperOverflowMenu.setUpHeplerEvents();
