615737bd559b27296ea3ef1e8bf62459aa05277e
Mayank Gupta <mayankgupta@Mayanks-MacBook-Pro.local>
2019-05-23 20:52:51 +0530

    removed version

--- js/utils/config.js
+++ js/utils/config.js
@@ -1,4 +1,3 @@
 export const PREFIX = "hcl";
-export const VERSION = "0.0.1";
 export const weekDays = ['S', 'M', 'T', 'W', 'Th', 'F', 'S'];
 export const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
