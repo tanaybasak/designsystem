3ccfa5ab18f6dfac26b5dbc0f9c21c20de73f346
Vivek Ananth <vivekananth@Viveks-MacBook-Pro.local>
2019-06-19 20:45:35 +0530

    pagination changes: pageItems event added

--- js/pagination.js
+++ js/pagination.js
@@ -8,12 +8,14 @@ class Pagination {
         this.selectors = {
             next: `.${PREFIX}-pagination-button-next`,
             previous: `.${PREFIX}-pagination-button-previous`,
-            PageNumber: `.${PREFIX}-pagination-select.${PREFIX}-page-number`
+            PageNumber: `.${PREFIX}-pagination-select.${PREFIX}-page-number`,
+            PageItems: `.${PREFIX}-pagination-select.${PREFIX}-page-items`
         }
 
         this.events = {
             eventPageChange: 'pageChange',
             eventPageNumber: 'pageNumber',
+            eventPageItems: 'itemsPerPage',
         }
 
         this.buttons = {
@@ -22,7 +24,18 @@ class Pagination {
         }
 
         this.state = {
-            navigationButton: { clicked: false, which: this.buttons.NEXT },
+            navigationButton: {
+                clicked: false,
+                which: this.buttons.NEXT
+            },
+            pageNumber: {
+                clicked: false,
+                value: 1
+            },
+            pageItems: {
+                clicked: false,
+                value: 1
+            },
             ...options
         }
 
@@ -30,13 +43,17 @@ class Pagination {
     }
 
     toggleState = (state) => {
-        const { clicked, which } = state.navigationButton;
+        const { clicked: navigationClicked = false, which } = state.navigationButton;
         const { clicked: pageNumberClicked = false } = state.pageNumber;
-        if (clicked) {
+        const { clicked: pageItemsClicked = false } = state.pageItems;
+        if (navigationClicked) {
             this.emitEvent(this.events.eventPageChange, { 'direction': which });
         }
         if (pageNumberClicked) {
-            this.emitEvent(this.events.eventPageChange, { 'value': state.pageNumber.value });
+            this.emitEvent(this.events.eventPageNumber, { 'value': state.pageNumber.value });
+        }
+        if (pageItemsClicked) {
+            this.emitEvent(this.events.eventPageItems, { 'value': state.pageItems.value });
         }
     }
 
@@ -65,6 +82,8 @@ class Pagination {
         e.preventDefault();
         if (target && getClosest(target, this.selectors.PageNumber)) {
             this.emitEvent(this.events.eventPageNumber, { 'value': target.options[target.selectedIndex].value });
+        } else if (target && getClosest(target, this.selectors.PageItems)) {
+            this.emitEvent(this.events.eventPageItems, { 'value': target.options[target.selectedIndex].value });
         }
     }
 
@@ -72,9 +91,11 @@ class Pagination {
         const pageForward = this.element.querySelector(this.selectors.next);
         const PageBackward = this.element.querySelector(this.selectors.previous);
         const pageNumber = this.element.querySelector(this.selectors.PageNumber);
+        const pageItems = this.element.querySelector(this.selectors.PageItems);
         pageForward.addEventListener('click', this.handleNavigation.bind(this));
         PageBackward.addEventListener('click', this.handleNavigation.bind(this));
         pageNumber.addEventListener('change', this.handleChange.bind(this));
+        pageItems.addEventListener('change', this.handleChange.bind(this));
     }
 
     static handleDataAPI = () => {
@@ -90,7 +111,10 @@ class Pagination {
                         clicked: false,
                         value: 1
                     },
-                    itemsPerPage: 10
+                    pageItems: {
+                        clicked: false,
+                        value: 1
+                    }
                 };
 
                 if (getClosest(target, `.${PREFIX}-pagination-button-next`)) {
@@ -99,6 +123,8 @@ class Pagination {
                     defaultOption['navigationButton'] = { clicked: true, which: 'previous' };
                 } else if (getClosest(target, `.${PREFIX}-pagination-select.${PREFIX}-page-number`)) {
                     defaultOption['pageNumber'] = { clicked: true };
+                } else if (getClosest(target, `.${PREFIX}-pagination-select.${PREFIX}-page-items`)) {
+                    defaultOption['pageItems'] = { clicked: true };
                 }
 
                 return new Pagination(element, {
