932f3c41ab252d7a8465f73e1d4c0ef45ab3d4c0
Vatsal Bhardwaj <vatsal.b@hcl.com>
2020-01-23 15:40:42 +0530

    FIX #UICOE-643 [Fixed issue in FileUploader]

--- js/index.js
+++ js/index.js
@@ -14,6 +14,7 @@ import Accordion from './accordion';
 import Password from './password';
 import Slider from './slider';
 import Tree from './tree';
+import FileUploader from './fileUploader';
 const ComponentList = {
   dropdow: Dropdown,
   navigation: Navigation,
@@ -25,7 +26,8 @@ const ComponentList = {
   pagination: Pagination,
   password: Password,
   accordion: Accordion,
-  tree: Tree
+  tree: Tree,
+  fileUploader: FileUploader
 };
 
 for (const componentName in ComponentList) {
@@ -84,6 +86,9 @@ export const components = {
   },
   tree: function(selector) {
     attachElements(selector, null, Tree);
+  },
+  fileUploader: function(selector) {
+    attachElements(selector, null, FileUploader);
   }
 };
 
