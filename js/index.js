50b2cab129e59348be797e59a12a7bf1d7fa7d88
Vatsal Bhardwaj <vatsal.b@hcl.com>
2020-01-27 16:46:11 +0530

    FIX #UICOE-657 [Fixed issue in Data Table]

--- js/index.js
+++ js/index.js
@@ -14,6 +14,7 @@ import Accordion from './accordion';
 import Password from './password';
 import Slider from './slider';
 import Tree from './tree';
+import DataTable from './dataTable';
 const ComponentList = {
   dropdow: Dropdown,
   navigation: Navigation,
@@ -25,7 +26,8 @@ const ComponentList = {
   pagination: Pagination,
   password: Password,
   accordion: Accordion,
-  tree: Tree
+  tree: Tree,
+  dataTable: DataTable
 };
 
 for (const componentName in ComponentList) {
@@ -84,6 +86,9 @@ export const components = {
   },
   tree: function(selector) {
     attachElements(selector, null, Tree);
+  },
+  dataTable: function(selector) {
+    attachElements(selector, null, DataTable);
   }
 };
 
