ef15c8784fe652ea6d746e4655a8762a829cb44a
Delita <delitajosna.dsouza@hcl.com>
2020-02-17 11:32:51 +0530

    Keyboard Accessbility for OverflowMenu

--- js/overflow.js
+++ js/overflow.js
@@ -12,7 +12,6 @@ class Overflow {
     this.toggleState(this.state.isOpen);
   }
   
-
   toggleState = state => {
     if (state) {
        this.element.querySelector(".hcl-overflow-menu").classList.add(`${PREFIX}-show`);
@@ -23,8 +22,64 @@ class Overflow {
     }
   };
 
+  focusNode = node => {
+    if (node.classList.contains('hcl-overflow-option')) {
+      node.children[0].focus();
+    }
+  };
+
+  keyDownOnTree = (e) => {
+    let key = e.which || e.keyCode;
+    const nodeElement = e.currentTarget;
+    const listItem = e.target.parentElement;
+    const nodeStatus = nodeElement.classList.contains("hcl-show"); 
+    
+    if (nodeStatus === undefined || nodeStatus === null) {
+        nodeStatus = 'false';
+    }
+    if(nodeStatus){
+    switch (key) {
+      case 40: {
+        if(!listItem.nextElementSibling){
+          this.focusNode(listItem.parentElement.firstElementChild);
+        }
+        else if(listItem.nextElementSibling.children[0].disabled === true){
+          this.focusNode(listItem.nextElementSibling.nextElementSibling);
+        }
+        else{
+          this.focusNode(listItem.nextElementSibling);
+        }
+        e.preventDefault();
+        break;
+      }
+      case 38: {
+        if(!listItem.previousElementSibling){
+          this.focusNode(listItem.parentElement.lastElementChild);
+        }
+        else if(listItem.previousElementSibling.children[0].disabled === true){
+          this.focusNode(listItem.previousElementSibling.previousElementSibling);
+        }
+        else{
+          this.focusNode(listItem.previousElementSibling)
+        }
+        e.preventDefault();
+        break;
+      }
+      case 13: {
+        e.preventDefault();
+        e.target.click();
+        break;
+      }
+      default:
+      break;
+    }
+  }
+  }
+
   attachEvents = () => {
     const icon = this.element.querySelector('.hcl-ellipsis');
+    const overflowMenu = this.element.querySelector('.hcl-overflow-menu');
+
     trackDocumentClick(this.element, () => {
       if (this.state.isOpen) {
         this.state.isOpen = !this.state.isOpen;
@@ -39,6 +94,10 @@ class Overflow {
           icon.click();
         }
       });
+
+      overflowMenu.addEventListener('keydown', e => {
+        this.keyDownOnTree(e, "true");
+      });
     
       icon.addEventListener('click', event => {
         event.stopPropagation();
@@ -50,6 +109,7 @@ class Overflow {
         });
         this.state.isOpen = !this.state.isOpen;
         this.toggleState(this.state.isOpen);
+        this.focusNode(overflowMenu.children[0].children[0]);
       }); 
 
       this.element.querySelectorAll(`.${PREFIX}-overflow-option`)
@@ -59,7 +119,7 @@ class Overflow {
               this.state.onChange(event, event.target.innerText);
             }
           });
-      });
+        });
     };
   }
 }
