5c57254645e5d04a04555a81a23fd98a5cc46716
Delita <delitajosna.dsouza@hcl.com>
2020-02-27 16:25:58 +0530

    format related changes

--- js/modal.js
+++ js/modal.js
@@ -1,76 +1,81 @@
 import handleDataBinding from './utils/data-api';
 class Modal {
-  constructor(element,options) {
+  constructor(element, options) {
     this.element = element;
-   
+
     this.state = {
-        isOpen: false,
-        ...options
-      };       
+      isOpen: false,
+      ...options
+    };
   }
 
-  focusTrap = (e) => {
-        let modal = document.getElementById(this.element.getAttribute('data-target').slice(1));
-        let focusableEls = modal.querySelectorAll('a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input[type="text"]:not([disabled]), input[type="radio"]:not([disabled]), input[type="checkbox"]:not([disabled]), select:not([disabled])');
-        let firstFocusableEl = focusableEls[0];  
-        let lastFocusableEl = focusableEls[focusableEls.length - 1];
-        
-        if (event.keyCode == 27){
-            event.preventDefault();
-            this.hideModal(modal);
-        }
-    
-        let isTabPressed = (e.key === 'Tab' || e.keyCode === "9");
+  focusTrap = e => {
+    const modal = document.getElementById(
+      this.element.getAttribute('data-target').slice(1)
+    );
+    const focusableEls = modal.querySelectorAll(
+      'a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input[type="text"]:not([disabled]), input[type="radio"]:not([disabled]), input[type="checkbox"]:not([disabled]), select:not([disabled]), [tabindex]'
+    );
+    const firstFocusableEl = focusableEls[0];
+    const lastFocusableEl = focusableEls[focusableEls.length - 1];
 
-        if (!isTabPressed) { 
-            return; 
-        }
+    if (event.keyCode === 27) {
+      event.preventDefault();
+      this.hideModal(modal);
+    }
 
-        if ( e.shiftKey ){
-            if (document.activeElement === firstFocusableEl) {
-                lastFocusableEl.focus();
-                e.preventDefault();
-            }
-        } else {
-            if (document.activeElement === lastFocusableEl) {
-                firstFocusableEl.focus();
-                e.preventDefault();
-            }
-        }
-    };
+    const isTabPressed = e.key === 'Tab' || e.keyCode === '9';
 
-  showModal = (modal) => { 
-    if(modal ==null)return
+    if (!isTabPressed) {
+      return;
+    }
+
+    if (e.shiftKey) {
+      if (document.activeElement === firstFocusableEl) {
+        lastFocusableEl.focus();
+        e.preventDefault();
+      }
+    } else {
+      if (document.activeElement === lastFocusableEl) {
+        firstFocusableEl.focus();
+        e.preventDefault();
+      }
+    }
+  };
+
+  showModal = modal => {
+    if (modal == null) return;
     modal.classList.remove('hcl-modal-hide');
     modal.focus();
   };
 
-  hideModal = (modal) => {
-    if(modal ==null)return
+  hideModal = modal => {
+    if (modal == null) return;
     modal.classList.add('hcl-modal-hide');
   };
 
   attachEvents = () => {
-    let modal = document.getElementById(this.element.getAttribute('data-target').slice(1));
+    const modal = document.getElementById(
+      this.element.getAttribute('data-target').slice(1)
+    );
     const closeModalBtn = modal.querySelectorAll('[data-dismiss-btn]');
 
-    if(this.state.isOpen){
-        //this.state.isOpen = !this.state.isOpen;
-        this.showModal(modal);
+    if (this.state.isOpen) {
+      this.showModal(modal);
     }
 
     modal.addEventListener('keydown', this.focusTrap);
 
     this.element.addEventListener('click', () => {
-        this.showModal(modal);  
-    })  
-    
+      this.showModal(modal);
+    });
+
     closeModalBtn.forEach(button => {
-        button.addEventListener('click', () => {
-           let modal = button.closest('.hcl-modal');
-           this.hideModal(modal);    
-        })      
-     });
+      button.addEventListener('click', () => {
+        const modal = button.closest('.hcl-modal');
+        this.hideModal(modal);
+      });
+    });
   };
 
   static handleDataAPI = () => {
