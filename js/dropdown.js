77233b6bbf009ec3a88fb00c5acd7de9afaa9233
Delita <delitajosna.dsouza@hcl.com>
2020-03-11 14:21:04 +0530

    keyboard accessibility is missing in dropdown

--- js/dropdown.js
+++ js/dropdown.js
@@ -46,37 +46,111 @@ class Dropdown {
     subElement.classList.add(className);
   };
 
-  attachEvents = () => {
-    trackDocumentClick(this.element, () => {
-      if (this.state.isOpen) {
-        this.toggleState(!this.state.isOpen);
-        this.state.isOpen = !this.state.isOpen;
+  focusNode = node => {
+    if (node.classList.contains(`${PREFIX}-dropdown-item`)) {
+      node.children[0].focus();
+    }
+  };
+
+  keyDownOnTree = e => {
+    const key = e.which || e.keyCode;
+    const listItem = e.target.parentElement;
+    const nodeStatus = this.element.classList.contains(
+      `${PREFIX}-dropdown-open`
+    );
+
+    if (nodeStatus) {
+      switch (key) {
+        case 40: {
+          if (!listItem.nextElementSibling) {
+            this.focusNode(listItem.parentElement.firstElementChild);
+          } else if (listItem.nextElementSibling.disabled === true) {
+            this.focusNode(listItem.nextElementSibling.nextElementSibling);
+          } else {
+            this.focusNode(listItem.nextElementSibling);
+          }
+          e.preventDefault();
+          break;
+        }
+        case 38: {
+          if (!listItem.previousElementSibling) {
+            this.focusNode(listItem.parentElement.lastElementChild);
+          } else if (listItem.previousElementSibling.disabled === true) {
+            this.focusNode(
+              listItem.previousElementSibling.previousElementSibling
+            );
+          } else {
+            this.focusNode(listItem.previousElementSibling);
+          }
+          e.preventDefault();
+          break;
+        }
+        case 13: {
+          e.preventDefault();
+          e.target.click();
+          break;
+        }
+        default:
+          break;
       }
-    });
-    this.element.addEventListener('click', event => {
-      event.stopPropagation();
+    }
+  };
+
+  attachEvents = () => {
+    const dropdownBtn = this.element.querySelector(
+      `.${PREFIX}-dropdown-toggle`
+    );
+    const dropdownMenu = this.element.querySelector(
+      `.${PREFIX}-dropdown-container`
+    );
+
+    if (dropdownBtn) {
+      dropdownBtn.addEventListener('keypress', function(event) {
+        if (event.keyCode === 13) {
+          event.preventDefault();
+          dropdownBtn.click();
+        }
+      });
+
+      dropdownMenu.addEventListener('keydown', e => {
+        this.keyDownOnTree(e);
+      });
+
       trackDocumentClick(this.element, () => {
         if (this.state.isOpen) {
           this.toggleState(!this.state.isOpen);
           this.state.isOpen = !this.state.isOpen;
         }
       });
-      this.state.isOpen = !this.state.isOpen;
-      this.toggleState(this.state.isOpen);
-    });
-
-    this.element
-      .querySelectorAll(`.${PREFIX}-dropdown-item`)
-      .forEach((item, index) => {
-        item.addEventListener('click', event => {
-          this.setSelection(event.target, `.${PREFIX}-dropdown-item-selected`);
-          this.setValue(event.target.innerText);
-          this.state.selected = index;
-          if (typeof this.state.onChange === 'function') {
-            this.state.onChange(event, event.target.innerText);
+      dropdownBtn.addEventListener('click', event => {
+        event.stopPropagation();
+        trackDocumentClick(this.element, () => {
+          if (this.state.isOpen) {
+            this.toggleState(!this.state.isOpen);
+            this.state.isOpen = !this.state.isOpen;
           }
         });
+        this.state.isOpen = !this.state.isOpen;
+        this.toggleState(this.state.isOpen);
+        this.focusNode(dropdownMenu.children[0]);
       });
+
+      this.element
+        .querySelectorAll(`.${PREFIX}-dropdown-item`)
+        .forEach((item, index) => {
+          item.addEventListener('click', event => {
+            this.setSelection(
+              event.target,
+              `.${PREFIX}-dropdown-item-selected`
+            );
+            this.setValue(event.target.innerText);
+            this.state.selected = index;
+            if (typeof this.state.onChange === 'function') {
+              this.state.onChange(event, event.target.innerText);
+            }
+          });
+        });
+    }
   };
 
   static handleDataAPI = () => {
