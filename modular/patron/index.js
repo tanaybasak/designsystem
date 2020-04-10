ef81f6545231b15ab9e96d9535134c500504663d
Vivek Vijayan <vivekvijayan@Viveks-MacBook-Pro.local>
2020-04-10 16:28:30 +0530

    treeview enhancement

--- modular/patron/index.js
+++ modular/patron/index.js
@@ -1,4 +1,5 @@
 import '../../scss/main.scss';
+import 'patron-icon/dist/patron-font.css';
 import ContentSwitcher from '../../js/content-switcher';
 import Dropdown from '../../js/dropdown';
 import Sidebar from '../../js/sidebar';
