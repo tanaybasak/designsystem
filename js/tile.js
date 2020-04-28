bcf70e83df0440b4ba8723e45f604ae867b22374
Delita <delitajosna.dsouza@hcl.com>
2020-04-28 19:54:27 +0530

    tile keyboard access fixes

--- js/tile.js
+++ js/tile.js
@@ -5,21 +5,36 @@ class Tile {
     this.element = element;
   }
 
-  attachEvents = () => {
-    this.element.addEventListener('keydown', e => {
+  keyDownOnTile = (e) => {
+    const key = e.which || e.keyCode;
+    const input = this.element.querySelector('input[type="checkbox"]');
+    if (key === 13) {
       e.preventDefault();
-      const input = this.element.querySelector('input[type="checkbox"]');
-      const clickTag = this.element.querySelector(`#${PREFIX}-tile-clickTag`);
-      const key = e.which || e.keyCode;
-      if (key === 13 || key === 32) {
-        if (input) {
-          input.checked = !input.checked;
-        }
-        if (clickTag) {
-          clickTag.click();
-        }
+      if (input) {
+        input.checked = !input.checked;
       }
-    });
+    }
+  };
+
+  attachEvents = () => {
+    const expandable = this.element.querySelector(`.${PREFIX}-tile-arrow`);
+    const selectable = this.element.querySelector(`.${PREFIX}-tile-selectable`);
+
+    if (expandable) {
+      expandable.addEventListener('keydown', (e) => {
+        this.keyDownOnTile(e);
+      });
+    }
+    if (selectable) {
+      selectable.addEventListener('keydown', (e) => {
+        this.keyDownOnTile(e);
+      });
+    } else {
+      const link = this.element.querySelector(`#${PREFIX}-tile-clickTag`);
+      this.element.addEventListener('keydown', () => {
+        link.click();
+      });
+    }
   };
 }
 export default Tile;
