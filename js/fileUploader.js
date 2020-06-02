5ea9ca6ac3237dc0637c9373c978cf88304bfad0
Delita <delitajosna.dsouza@hcl.com>
2020-06-02 15:03:35 +0530

    fileuploader changes

--- js/fileUploader.js
+++ js/fileUploader.js
@@ -37,12 +37,16 @@ class FileUploader {
   getList = (event) => {
     const files = event.target.files;
     const length = event.target.files.length;
+    const multiple = this.element.querySelector(this.selectors.input).multiple;
+    if (!multiple) {
+      this.fileContainer.innerHTML = '';
+    }
     if (files) {
       for (let i = 0; i < length; i++) {
         const string = this.fileNameHTML(files[i].name);
         this.fileContainer.insertAdjacentHTML('beforeend', string);
       }
-      this.fileList = [...files, ...this.fileList];
+      this.fileList = multiple ? [...files, ...this.fileList] : [...files];
     }
     if (typeof this.state.onChange === 'function') {
       this.state.onChange(this.fileList);
