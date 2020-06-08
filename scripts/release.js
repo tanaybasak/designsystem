0c60c57accb3c3949d92b4d5de8addc180213ee5
Vivek Ananth <ananthv@hcl.com>
2020-06-08 09:06:34 +0530

    ok

--- scripts/release.js
+++ scripts/release.js
@@ -29,90 +29,91 @@ const PROCESS_DESC = '';
 //     return deferred.promise;
 // }
 
-// const getVersion = () => {
-//     let PROCESS_VER = process.env.version;
-//     let PROCESS_DESC = process.env.desc;
-//     PROCESS_VER = PROCESS_VER.replace(/\n/gi, '');
+const getVersion = () => {
+  let PROCESS_VER = process.env.version;
+  let PROCESS_DESC = process.env.desc;
+  PROCESS_VER = PROCESS_VER.replace(/\n/gi, '');
 
-//   console.log(`Version To Update: ${PROCESS_VER}`);
-//     return q.when(PROCESS_VER);
-// }
+  console.log(`Version To Update: ${PROCESS_VER}`);
+  process.exit(0);
+  return q.when(PROCESS_VER);
+}
 
 
 const bump = (version) => {
-    replace({
-        regex: /"version": "[^"]+"/m,
-        replacement: `"version": "${version}"`,
-        paths: [packageFile],
-        recursive: false,
-    });
-    return q.when(version);
+  replace({
+    regex: /"version": "[^"]+"/m,
+    replacement: `"version": "${version}"`,
+    paths: [packageFile],
+    recursive: false,
+  });
+  return q.when(version);
 }
 
 function addAndCommit(version) {
-    if (!version) {
-        console.log('No valid version!');
-        return deferred.reject(new Error(`Problem in ${version}`));
-    }
+  if (!version) {
+    console.log('No valid version!');
+    return deferred.reject(new Error(`Problem in ${version}`));
+  }
 
-    var deferred = q.defer();
-    git.pull('origin', `${BRANCH_NAME}`, (err1, result1) => {
-        if (!err1) {
-            console.log(`GIT:Adding...`);
-            git.add('./*', (err2, result2) => {
-                if (!err2) {
-                    console.log(`GIT:Committing...`);
-                    git.commit(`chore(release): ${version}`, [packageFile], (err3, result3) => {
-                        if (!err3) {
-                            console.log('Tagging...');
-                            if (PROCESS_DESC != "") {
-                                DESCRIPTION = PROCESS_DESC;
-                            }
-                            git.addAnnotatedTag(`v${version}`, `${DESCRIPTION}`, () => deferred.resolve(version));
-                        } else {
-                            return deferred.reject(new Error(`GIT:Commit: Issue`));
-                        }
-                    });
-                } else {
-                    return deferred.reject(new Error(`GIT:Add: Issue`));
-                }
-            })
+  var deferred = q.defer();
+  git.pull('origin', `${BRANCH_NAME}`, (err1, result1) => {
+    if (!err1) {
+      console.log(`GIT:Adding...`);
+      git.add('./*', (err2, result2) => {
+        if (!err2) {
+          console.log(`GIT:Committing...`);
+          git.commit(`chore(release): ${version}`, [packageFile], (err3, result3) => {
+            if (!err3) {
+              console.log('Tagging...');
+              if (PROCESS_DESC != "") {
+                DESCRIPTION = PROCESS_DESC;
+              }
+              git.addAnnotatedTag(`v${version}`, `${DESCRIPTION}`, () => deferred.resolve(version));
+            } else {
+              return deferred.reject(new Error(`GIT:Commit: Issue`));
+            }
+          });
         } else {
-            return deferred.reject(new Error(`GIT:Pull Issue`));
+          return deferred.reject(new Error(`GIT:Add: Issue`));
         }
-    })
-    return deferred.promise;
+      })
+    } else {
+      return deferred.reject(new Error(`GIT:Pull Issue`));
+    }
+  })
+  return deferred.promise;
 }
 
 function pushToBranch(version) {
-    let deferred = q.defer();
+  let deferred = q.defer();
 
-    console.log(`GIT:Push --> ${BRANCH_NAME}`);
-    git.push(['origin', `HEAD:refs/heads/${BRANCH_NAME}`], (err, result) => {
-        if (!err) {
-            console.log(`GIT:Push:Tags`);
-            git.pushTags(`origin`, () => {
-                return deferred.resolve(version);
-            });
-        } else {
-            return deferred.reject(new Error(`**** Issue in Pushing to ${BRANCH_NAME} / Issue in Pushing Tags ****`));
-        }
-    });
-    return deferred.promise;
+  console.log(`GIT:Push --> ${BRANCH_NAME}`);
+  git.push(['origin', `HEAD:refs/heads/${BRANCH_NAME}`], (err, result) => {
+    if (!err) {
+      console.log(`GIT:Push:Tags`);
+      git.pushTags(`origin`, () => {
+        return deferred.resolve(version);
+      });
+    } else {
+      return deferred.reject(new Error(`**** Issue in Pushing to ${BRANCH_NAME} / Issue in Pushing Tags ****`));
+    }
+  });
+  return deferred.promise;
 }
 
 // getVersion()
 // dummy()
 getVersion()
-    .then((version) => {
-        return bump(version);
-    })
-    .then((version) => {
-        return addAndCommit(version);
-    })
-    .then((version) => {
-        return pushToBranch(version);
-    })
-    .done(() => {
-        console.log("Tasks Completed");
-    })
+  .then((version) => {
+    return bump(version);
+  })
+  .then((version) => {
+    return addAndCommit(version);
+  })
+  .then((version) => {
+    return pushToBranch(version);
+  })
+  .done(() => {
+    console.log("Tasks Completed");
+  })
