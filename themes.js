4f38a7637489b920e4c5513d1f4252ef4edf7989
Vivek Vijayan <vijayanv@hcl.com>
2020-07-30 17:07:01 +0530

    Revert "Uicoe 1013 notification"

--- themes.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('theme-gbl').addEventListener('change', (event) => {
-  if (event.target.checked) {
-    document.getElementById('main-wrapper').classList.remove('theme-light');
-    document.getElementById('main-wrapper').classList.add('theme-dark');
-  } else {
-    document.getElementById('main-wrapper').classList.remove('theme-dark');
-    document.getElementById('main-wrapper').classList.add('theme-light');
-  }
-});
