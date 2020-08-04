6acfa4be3b5cc2bc48a58683c68e8ec75dfb5796
Vivek Ananth <ananthv@hcl.com>
2020-08-04 13:40:50 +0530

    update rules same as react, angular

--- prettier.config.js
+++ prettier.config.js
@@ -4,5 +4,10 @@ module.exports = {
   tabWidth: 2,
   semi: true,
   singleQuote: true,
-  arrowParens: 'avoid'
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  proseWrap: 'preserve',
+  htmlWhitespaceSensitivity: 'css',
+  endOfLine: 'auto',
+  quoteProps: 'as-needed'
 };
